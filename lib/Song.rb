6bae50160b9f92d3b95eafc6d19bc02d3aaab3ea
William West <wwest466@gmail.com>
2020-09-26 23:34:53 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -55,7 +55,7 @@ def self.new_from_filename(name)
   file_name = name.split(" - ")
   name = file_name[1]
   artist = file_name[0]
-  
+
   song = Song.create(name)
   song.artist = Artist.new(file_name[0])
 end
